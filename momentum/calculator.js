83613ca97a0c00588bfb223174bbe6d525051104
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-04-18 21:42:28 +0900

    fix: Add practice files before starting Momentom project

---
+++ momentum/calculator.js
@@ -0,0 +1,19 @@
+const calculator = {
+    add: function (a, b) {
+        console.log(a + b);
+    },
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+    divide: function (a, b) {
+        console.log(a / b);
+    },
+    powerOf: function (a, b) {
+        console.log(a * b);
+    }
+};
+
+calculator.add(5, 1)
+calculator.minus(5, 1)
+calculator.divide(5, 1)
+calculator.powerOf(5, 1)
