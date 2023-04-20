b1794f8afc75912d38fc7164203908ff51646edd
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-04-20 23:48:51 +0900

    docs: Add Conditional concepts practice

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-const age = prompt("How old are you?") 
-// prompt 함수는 Javascript의 실행을 멈추게 할 뿐더러, CSS를 적용시킬 수도 없기에 잘 쓰진 않음
+// conditional practice
 
-console.log(age);
+const age = parseInt(prompt("How old are you?"));
+
+if(isNaN(age)) { // 1. true고
+    console.log("Please write a number");
+} else if (age < 18) { // 2. true여야
+    console.log("You are too young")
+} else if (age >= 18 && age <= 50) { // 3. 이 조건들 만족 여부 판단 가능
+    console.log("You can drink");
+} else if (age > 50 && age <= 80) {
+    console.log("You should exercise");
+} else {
+    console.log("You can't drink");
+}
