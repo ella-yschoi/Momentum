627dc3dd5cf1515e00e98a360f7ebd2352aca2fb
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-04-20 23:58:08 +0900

    fix: Edit Conditional concepts practice

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 
 const age = parseInt(prompt("How old are you?"));
 
-if(isNaN(age)) { // 1. true고
-    console.log("Please write a number");
+if(isNaN(age || age < 0)) { // 1. true고
+    console.log("Please write a real positive number");
 } else if (age < 18) { // 2. true여야
     console.log("You are too young")
 } else if (age >= 18 && age <= 50) { // 3. 이 조건들 만족 여부 판단 가능
@@ -11,5 +11,5 @@ if(isNaN(age)) { // 1. true고
 } else if (age > 50 && age <= 80) {
     console.log("You should exercise");
 } else {
-    console.log("You can't drink");
+    console.log("You can do whatever you want!");
 }
