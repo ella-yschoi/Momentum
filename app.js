373d05f38c4925fc9e09f7b09a567c1552c6493d
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-04-25 21:03:45 +0900

    fix: Add annotation

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const title = document.querySelector("div.hello:first-child h1")
 
 function handleTitleClick() {
-    console.log("Title was clicked");
+    title.style.color = "blue";
 }
 
 title.addEventListener("click", handleTitleClick);
+// 1. addEventListener 호출
+// 2. listen 하고 싶은 event 이름 알려주기
+// 3. event가 발생하면 호출할 function을 전달해 주기
