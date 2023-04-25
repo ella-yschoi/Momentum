360bd8c81a41ad9a3587b5fc402d3d9f4c5801ce
Ella Yeonsu Choi <happymondaysu@gmail.com>
2023-04-25 20:13:16 +0900

    feat: Add QuerySelector concepts

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// conditional practice
+const title = document.querySelector(".hello h1");
 
-const age = parseInt(prompt("How old are you?"));
-
-if(isNaN(age || age < 0)) { // 1. true고
-    console.log("Please write a real positive number");
-} else if (age < 18) { // 2. true여야
-    console.log("You are too young")
-} else if (age >= 18 && age <= 50) { // 3. 이 조건들 만족 여부 판단 가능
-    console.log("You can drink");
-} else if (age > 50 && age <= 80) {
-    console.log("You should exercise");
-} else {
-    console.log("You can do whatever you want!");
-}
+title.innerText = "Hello";
