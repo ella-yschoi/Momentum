e7416ebd748e3b1f3b9f9a7bfcbcdbfa3db90787
ella-yschoi <happymondaysu@gmail.com>
2023-04-26 18:22:56 +0900

    fix: Edit previous document

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const h1 = document.querySelector("div.hello:first-child h1")
 // selector와 일치하는 문서 내 첫번째 Element를 반환, 없으면 null 반환
 // 매개변수는 유효한 CSS selector여야만 함
 
-function handleh1Click() { 
+function handleTitleClick() { 
     h1.style.color = "blue"; 
 }
 
@@ -30,7 +30,7 @@ function handleWindoOnline() {
     alert("Here is Wifi");
 }
 
-h1.addEventListener = ("click", handleh1Click);
+h1.addEventListener ("click", handleTitleClick);
 h1.addEventListener("mouseenter",handleMouseEnter);
 h1.addEventListener("mouseleave", handleMouseLeave);
 
