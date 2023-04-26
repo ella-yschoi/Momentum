78875cf21c7af397aa1c005004b2d91d1110658e
ella-yschoi <happymondaysu@gmail.com>
2023-04-26 18:14:36 +0900

    feat: Add another addEventListener

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-const title = document.querySelector("div.hello:first-child h1")
+const h1 = document.querySelector("div.hello:first-child h1")
 // selector와 일치하는 문서 내 첫번째 Element를 반환, 없으면 null 반환
 // 매개변수는 유효한 CSS selector여야만 함
 
-function handleTitleClick() { // title click시
-    title.style.color = "blue"; // title color가 blue로 바뀜
+function handleh1Click() { 
+    h1.style.color = "blue"; 
 }
 
-function handleMouseEnter() { // mouse를 갖다대면
-    title.innerText = "Mouse is here"; // 해당 text로 바뀜
+function handleMouseEnter() { 
+    h1.innerText = "Mouse is here"; 
 }
 
-function handleMouseLeave() { // mouse를 갖다대지 않으면
-    title.innerText = "Mouse is not here"; // 해당 text로 바뀜
+function handleMouseLeave() { 
+    h1.innerText = "Mouse is not here"; 
 }
 
-// title이, 특정 event(클릭, 호버 등)를, listen 할 때마다, 호출할 함수
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter",handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
+function handleWindowResize() {
+    document.body.style.backgroundColor = "skyblue";
+}
+
+function handleWindowCopy() {
+    alert("Hey, Don't copy that");
+}
+
+function handleWindoOffline() {
+    alert("Where is Wifi?");
+}
+
+function handleWindoOnline() {
+    alert("Here is Wifi");
+}
+
+h1.addEventListener = ("click", handleh1Click);
+h1.addEventListener("mouseenter",handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindoOffline);
+window.addEventListener("online", handleWindoOnline);
