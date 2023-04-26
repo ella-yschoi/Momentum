8d3a87a99839acdfea2e2426c7c1376df5f8951d
ella-yschoi <happymondaysu@gmail.com>
2023-04-26 18:34:42 +0900

    feat: Add EventListener concepts with conditional statement

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1")
-// selector와 일치하는 문서 내 첫번째 Element를 반환, 없으면 null 반환
-// 매개변수는 유효한 CSS selector여야만 함
 
 function handleTitleClick() { 
-    h1.style.color = "blue"; 
+    const currentColor = h1.style.color
+    let newColor; // 비어있는 변수
+    if(currentColor === "skyblue") { // 일치하는지 확인해 true or false 반환
+        newColor = "pink"; // color가 skyblue라면 Pink로 변경하라
+    } else {
+        newColor = "skyblue"; // 아니라면 skyblue로 두어라
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter() { 
-    h1.innerText = "Mouse is here"; 
-}
-
-function handleMouseLeave() { 
-    h1.innerText = "Mouse is not here"; 
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "skyblue";
-}
-
-function handleWindowCopy() {
-    alert("Hey, Don't copy that");
-}
-
-function handleWindoOffline() {
-    alert("Where is Wifi?");
-}
-
-function handleWindoOnline() {
-    alert("Here is Wifi");
-}
-
-h1.addEventListener ("click", handleTitleClick);
-h1.addEventListener("mouseenter",handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindoOffline);
-window.addEventListener("online", handleWindoOnline);
+h1.addEventListener("click", handleTitleClick);
